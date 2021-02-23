747b37749003504b774aeaeda4db5e77510a1ec2
soon00sad <60748579+soon00sad@users.noreply.github.com>
2021-02-23 22:35:17 +0700

    Add files via upload

---
+++ CalculationOperations/Cos.cpp
@@ -0,0 +1,22 @@
+#include <cmath>
+#include "../CalculationOperations.h"
+#include "../OperationsCreator.h"
+#include "../CalculationExceptions.h"
+
+#define _USE_MATH_DEFINES
+
+class Cos : public CalculationOperations {
+public:
+    void run(std::list<std::string> &args, ExecutionContext &executionContext) override {
+        if (executionContext.stackIsEmpty()) {
+            throw BadArgumentsException("No operand for 'COS'");
+        }
+        if (!args.empty()) {
+            throw BadArgumentsException("Too much arguments for 'COS'");
+        }
+        double num = (M_PI * executionContext.popStack())/180;
+        executionContext.pushStack(cos(num));
+    }
+};
+
+CREATE_OPERATION(Cos, COS)
